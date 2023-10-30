c56814159b428600d065d9639c1efe20f8fd6f54
Cristian Rodriguez Benitez <126631418+CritianRodriguez042502@users.noreply.github.com>
2023-10-30 12:15:58 -0500

    Agrando la url del proyecto de bbc-news

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,17 +1,11 @@
-import project from "../../assents/images/projects/projectDefault.png"
-import impactX from "../../assents/images/projects/IMPACT-X.png"
-import bbcNews from "../../assents/images/projects/BBC-NEWS.png"
-import galery from "../../assents/images/projects/GALERY.png"
-import calculator from "../../assents/images/projects/CALCULATOR.png"
+import project from "../../assents/images/projects/projectDefault.png";
+import impactX from "../../assents/images/projects/IMPACT-X.png";
+import bbcNews from "../../assents/images/projects/BBC-NEWS.png";
+import galery from "../../assents/images/projects/GALERY.png";
+import calculator from "../../assents/images/projects/CALCULATOR.png";
 import style from "./style_projects.module.css";
 
 export function Projects() {
-  
-  function navigateImpactX(e) {
-    window.location.href =
-      "https://critianrodriguez042502.github.io/agency_view/";
-  }
-
   return (
     <main id="projects">
       <section className={style.containerProjects}>
@@ -20,33 +14,27 @@ export function Projects() {
         </aside>
         <aside className={style.containerProjects}>
           <div className={style.containerImg}>
-            <img
-              onClick={navigateImpactX}
-              className={style.Img}
-              src={impactX}
-              alt="img"
-            />
+            <a
+              href="https://critianrodriguez042502.github.io/agency_view/"
+              target="_blank"
+            >
+              <img className={style.Img} src={impactX} alt="img" />
+            </a>
           </div>
           <div className={style.containerImg}>
-            <img
-              className={style.Img}
-              src={bbcNews}
-              alt="img"
-            />
+            <a href="https://bbc-news-api.onrender.com/" target="_blank">
+              <img className={style.Img} src={bbcNews} alt="img" />
+            </a>
           </div>
           <div className={style.containerImg}>
-            <img
-              className={style.Img}
-              src={galery}
-              alt="img"
-            />
+            <a target="_blank">
+              <img className={style.Img} src={galery} alt="img" />
+            </a>
           </div>
           <div className={style.containerImg}>
-            <img
-              className={style.Img}
-              src={calculator}
-              alt="img"
-            />
+            <a target="_blank">
+              <img className={style.Img} src={calculator} alt="img" />
+            </a>
           </div>
         </aside>
       </section>
