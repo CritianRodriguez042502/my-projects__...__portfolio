1ce8e469c9c0a4d5476b6ba3e91f6d95cd84a2bc
Cristian Rodriguez <126631418+CritianRodriguez042502@users.noreply.github.com>
2024-12-02 12:13:04 -0500

    Update Projects.jsx

--- src/components/portfolio-components/projects/Projects.jsx
+++ src/components/portfolio-components/projects/Projects.jsx
@@ -64,7 +64,7 @@ export function Projects() {
                                 <button className={style.projectButton}> Git hub </button>
                             </a>
                             <a
-                                href="#"
+                                href="https://rijksmuseum-app.onrender.com"
                                 target="_blank"
                                 rel="noreferrer"
                             >
